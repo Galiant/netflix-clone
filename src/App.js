b9b5aaab606f827ef967b641c2181781313df0b7
Galiant <antonijodev@gmail.com>
2020-11-14 22:15:50 +0000

    Starting with routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FooterContainer from './containers/footer';
 import JumbotronContainer from './containers/jumbotron';
 import FaqsContainer from './containers/faqs';
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
     <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+      <Router>
+        <Route exact path={ROUTES.HOME}>
+          <JumbotronContainer />
+          <FaqsContainer />
+          <FooterContainer />
+        </Route>
+      </Router>
     </>
   );
 }
