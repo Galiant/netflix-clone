8d8fa24273b78cd1255289eaf71961814f343197
Galiant <antonijodev@gmail.com>
2020-11-15 20:23:13 +0000

    Adding other pages and routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import FooterContainer from './containers/footer';
-import JumbotronContainer from './containers/jumbotron';
-import FaqsContainer from './containers/faqs';
 import * as ROUTES from './constants/routes';
+import { Home, Browse, Signin, Signup } from './pages';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Route exact path={ROUTES.HOME}>
-          <JumbotronContainer />
-          <FaqsContainer />
-          <FooterContainer />
-        </Route>
-      </Router>
-    </>
+    <Router>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
