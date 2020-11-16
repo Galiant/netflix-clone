bc3429828d22608e8305ce3a85d7ee7023013048
Galiant <antonijodev@gmail.com>
2020-11-16 22:24:40 +0000

    Starting to work on header component

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+export const Background = styled.div``;
+
+export const Frame = styled.div``;
+
+export const ButtonLink = styled(ReactRouterLink)``;
+
+export const Container = styled.div``;
+
+export const Logo = styled.img``;
