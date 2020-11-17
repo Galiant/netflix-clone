ff221dbc82273760876035761b93565f84827610
Galiant <antonijodev@gmail.com>
2020-11-17 21:56:16 +0000

    Adding background image and opt form to header container

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { OptForm } from '../components';
 import { HeaderContainer } from '../containers/header';
 import FooterContainer from '../containers/footer';
 import JumbotronContainer from '../containers/jumbotron';
@@ -8,10 +9,19 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <OptForm>
+          <OptForm.Input placeholder='Email address' />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </OptForm.Text>
+        </OptForm>
       </HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
     </>
   );
 }
