e2a60c94b77542b2a3a3f8680df7fe6029c4399c
Galiant <antonijodev@gmail.com>
2020-11-21 22:49:50 +0000

    Adding form to sign in page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,45 @@
-import React from 'react';
+/*eslint-disable no-unused-vars*/
+import React, { useState } from 'react';
 import { HeaderContainer } from '../containers/header';
 import FooterContainer from '../containers/footer';
+import { Form } from '../components';
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('');
+
+  // could use here more checks for password and email
+  const isInvalid = password === '' || emailAddress === '';
+  const handleSignIn = event => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <HeaderContainer>This is Sign In page!</HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method='POST'>
+            <Form.Input
+              placeholder='Email address'
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Password
+              type='password'
+              placeholder='Password'
+              autoComplete='off'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type='submit'>
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
