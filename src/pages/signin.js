79ffe86cf7078a07f720d9461b8177f948a60e09
Galiant <antonijodev@gmail.com>
2020-11-23 22:15:38 +0000

    Introducing firebase for sign in page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,18 +1,35 @@
-/*eslint-disable no-unused-vars*/
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { HeaderContainer } from '../containers/header';
 import FooterContainer from '../containers/footer';
 import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function SignIn() {
-  const [emailAddress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   // could use here more checks for password and email
   const isInvalid = password === '' || emailAddress === '';
   const handleSignIn = event => {
     event.preventDefault();
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        // push to browser page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch(error => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
   };
 
   return (
