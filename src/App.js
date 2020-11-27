9657f2d432fc5b3b6fea0ac71668cbc577262317
Galiant <antonijodev@gmail.com>
2020-11-27 21:07:26 +0000

    Sign in and sign up user protection

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Browse, SignIn, SignUp } from './pages';
+import { IsUserRedirect } from './helpers/routes';
 
 function App() {
+  const user = {};
   return (
     <Router>
+      <Route path={ROUTES.SIGN_IN}>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+        >
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+        >
+          <SignUp />
+        </IsUserRedirect>
+      </Route>
       <Route exact path={ROUTES.BROWSE}>
         <Browse />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
-        <SignIn />
-      </Route>
-      <Route exact path={ROUTES.SIGN_UP}>
-        <SignUp />
       </Route>
       <Route exact path={ROUTES.HOME}>
         <Home />
