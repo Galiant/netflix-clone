faf38a242a2e23ddfa59f9fb9713014e6e441f36
Galiant <antonijodev@gmail.com>
2020-11-30 22:13:51 +0000

    Using custom hooks to retrieve content

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,2 @@
-export { dafault as userAuthListener } from './use-auth-listener';
+export { default as useAuthListener } from './use-auth-listener';
+export { default as useContent } from './use-content';
