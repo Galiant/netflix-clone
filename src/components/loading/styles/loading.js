e3259ae116a81001b8babac3dad50dcba7255d91
Galiant <antonijodev@gmail.com>
2020-12-08 20:25:51 +0000

    Starting with the loading component

---
+++ src/components/loading/styles/loading.js
@@ -0,0 +1,12 @@
+import styled, { createGlobalStyle } from 'styled-components/macro';
+
+export const LockBody = createGlobalStyle`
+body {
+    overflow: hidden;
+}
+`;
+
+export const ReleaseBody = createGlobalStyle`
+body {
+    overflow: visible
+}`;
