8a3f653cf89dbc707f212944f385eb590e8a392d
Galiant <antonijodev@gmail.com>
2020-12-09 21:08:00 +0000

    Adding styles for profiles

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,10 +1,16 @@
+/*eslint-disable react/prop-types*/
 import React from 'react';
+import { Spinner, LockBody, ReleaseBody, Picture } from './styles/loading';
 
 export default function Loading({ src, ...restProps }) {
   return (
-    <Spinner>
+    <Spinner {...restProps}>
       <LockBody />
       <Picture src={`/images/users/${src}.png`} />
     </Spinner>
   );
 }
+
+Loading.ReleaseBody = function LoadingReleaseBody() {
+  return <ReleaseBody />;
+};
