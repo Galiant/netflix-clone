12429fe1a389c2113d23c9120b8d89d7f4088f41
Galiant <antonijodev@gmail.com>
2020-12-10 20:41:22 +0000

    Showing header on browser page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -3,7 +3,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
-import { Loading } from '../components';
+import { Header, Loading } from '../components';
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -18,9 +18,13 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : null
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src='joker1'>
+        <p>Hello</p>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
   );
