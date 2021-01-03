d48e876c038b52583e6b65d5ac86505ae35c3d86
Galiant <antonijodev@gmail.com>
2021-01-03 20:43:56 +0000

    Adding styles for card component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -57,6 +57,26 @@ Card.Meta = function CardMeta({ children, ...restProps }) {
   return <Meta {...restProps}>{children}</Meta>;
 };
 
+Card.Item = function CardItem({ item, children, ...restProps }) {
+  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
+
+  return (
+    <Item
+      {...restProps}
+      onClick={() => {
+        setItemFeature(item);
+        setShowFeature(true);
+      }}
+    >
+      {children}
+    </Item>
+  );
+};
+
+Card.Image = function CardImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
+
 Card.Feature = function CardFeature({ children, category, ...restProps }) {
   const { showFeature, itemFeature, setShowFeature } = useContext(
     FeatureContext
@@ -80,31 +100,11 @@ Card.Feature = function CardFeature({ children, category, ...restProps }) {
           {itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}
         </Maturity>
         <FeatureText fontWeight='bold'>
-          {itemFeature.genre.chaAt(0).toUpperCase() +
+          {itemFeature.genre.charAt(0).toUpperCase() +
             itemFeature.genre.slice(1)}
         </FeatureText>
       </Group>
       {children}
     </Feature>
   ) : null;
-};
-
-Card.Item = function CardItem({ item, children, ...restProps }) {
-  const { setShowFeature, setItemFeature } = useContext(FeatureContext);
-
-  return (
-    <Item
-      {...restProps}
-      onClick={() => {
-        setItemFeature(item);
-        setShowFeature(true);
-      }}
-    >
-      {children}
-    </Item>
-  );
-};
-
-Card.Image = function CardImage({ ...restProps }) {
-  return <Image {...restProps} />;
 };
