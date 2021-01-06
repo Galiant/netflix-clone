eb5e63d10aab519469e82378d139dd8dc0e5bc32
Galiant <antonijodev@gmail.com>
2021-01-06 21:07:07 +0000

    Fix import path for styles in player component

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,7 +1,7 @@
 /*eslint-disable react/prop-types*/
 import React, { useState, useContext, createContext } from 'react';
 import ReactDOM from 'react-dom';
-import { Container, Button, Overlay, Inner, Close } from './styles/card';
+import { Container, Button, Overlay, Inner, Close } from './styles/player';
 
 export const PlayerContext = createContext();
 
