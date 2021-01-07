73561022299bd6435ed1ca4db32840337c7eb54d
Galiant <antonijodev@gmail.com>
2021-01-07 20:38:10 +0000

    Implementing search using fuse library

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,7 @@
 /*eslint-disable react/prop-types*/
 /*eslint-disable no-unused-vars*/
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 import { Header, Loading, Card, Player } from '../components';
@@ -27,6 +28,19 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre'],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
