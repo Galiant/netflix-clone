2ddda8b622fb9f7eaa5547ac2d5b5b8ee7771f30
Galiant <antonijodev@gmail.com>
2021-01-08 20:46:26 +0000

    Testing accordion

---
+++ src/__tests__/components/accordion.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import faqsData from '../../fixtures/faqs.json';
+import { Accordion } from '../../components';
+
+const WrapperComponent = () => {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqsData.map(item => (
+        <Accordion.Item key={item.id}>
+          <Accordion.Header>{item.header}</Accordion.Header>
+          <Accordion.Body>{item.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
+};
+
+describe('Accordion', () => {
+  it('renders the Accordion with populated data', () => {
+    const { container, getByText } = render(<WrapperComponent />);
+
+    expect(getByText('Frequently Asked Questions')).toBeTruthy();
+    expect(getByText('What is Netflix?')).toBeTruthy();
+    expect(getByText('How much does Netflix cost?')).toBeTruthy();
+    expect(getByText('Where can I watch?')).toBeTruthy();
+    expect(getByText('How do I cancel?')).toBeTruthy();
+    expect(getByText('What can I watch on Netflix?')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('opens and closes accordion', () => {
+    const { container, queryByText } = render(<WrapperComponent />);
+
+    const whatIsNetflixBodyText =
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+
+    expect(queryByText(whatIsNetflixBodyText)).toBeFalsy();
+    fireEvent.click(queryByText('What is Netflix?'));
+    expect(queryByText(whatIsNetflixBodyText)).toBeTruthy();
+    fireEvent.click(queryByText('What is Netflix?'));
+    expect(queryByText(whatIsNetflixBodyText)).toBeFalsy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
