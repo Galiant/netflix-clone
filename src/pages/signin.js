7a5f5e8712a183ed9632608a214c54b60db40f00
Galiant <antonijodev@gmail.com>
2021-01-25 20:16:47 +0000

    Testing signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -37,7 +37,7 @@ export default function SignIn() {
       <HeaderContainer>
         <Form>
           <Form.Title>Sign In</Form.Title>
-          {error && <Form.Error>{error}</Form.Error>}
+          {error && <Form.Error data-testid='error'>{error}</Form.Error>}
           <Form.Base onSubmit={handleSignIn} method='POST'>
             <Form.Input
               placeholder='Email address'
@@ -51,7 +51,11 @@ export default function SignIn() {
               value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type='submit'>
+            <Form.Submit
+              disabled={isInvalid}
+              type='submit'
+              data-testid='sign-in'
+            >
               Sign In
             </Form.Submit>
           </Form.Base>
